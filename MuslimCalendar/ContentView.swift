264efec4892ba932f3ad9abc40e4cfa6c7f339ac
Moustafa Abdelhamid <mou.abdelhamid@gmail.com>
2022-03-06 09:59:33 +0300

    Initial Commit

---
+++ MuslimCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuslimCalendar
+//
+//  Created by Mustafa Abdelhamıd on 6.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuslimCalendar/MuslimCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuslimCalendarApp.swift
+//  MuslimCalendar
+//
+//  Created by Mustafa Abdelhamıd on 6.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MuslimCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
