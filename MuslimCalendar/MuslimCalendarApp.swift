d585bf353d23e9b92b4ac2b8b8b86343d08af60a
Moustafa Abdelhamid <mou.abdelhamid@gmail.com>
2022-03-21 14:24:36 +0300

    Request a calendar permission

--- MuslimCalendar/MuslimCalendarApp.swift
+++ MuslimCalendar/MuslimCalendarApp.swift
@@ -6,14 +6,42 @@
 //
 
 import SwiftUI
+import EventKit
 
 @main
 struct MuslimCalendarApp: App {
+    init() {
+        print("happening")
+        requestPermission()
+    }
+    
     private let viewModel = ViewModel()
     
     var body: some Scene {
         WindowGroup {
             ContentView(viewModel: viewModel)
+        }
+    }
+    
+    func requestPermission() {
+        let eventStore = EKEventStore()
+        switch EKEventStore.authorizationStatus(for: .event) {
+        case .notDetermined:
+            eventStore.requestAccess(to: .event, completion: { (success, error) in
+                if success {
+                    print("success")
+                } else {
+                    print("error while requesting permession \(String(describing: error?.localizedDescription))")
+                }
+            })
+        case .restricted:
+            print("restriced")
+        case .denied:
+            print("denied")
+        case .authorized:
+            print("authorized")
+        @unknown default:
+            print("unkown")
         }
     }
 }
